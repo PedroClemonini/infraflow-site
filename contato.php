94fbc193bada292c6e5c635e845b352136782b87
Pedro Clemonini <pclemonini-00@hotmail.com>
2026-04-20 16:26:43 -0300

    mailer-config

--- contato.php
+++ contato.php
@@ -4,6 +4,9 @@ require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 $mensagem_status = "";
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
